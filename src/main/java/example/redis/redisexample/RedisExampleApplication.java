ce19deac9ea8e0671b25ab377b5ec9f2d68d2b70
jiho Shin <jhhj424@naver.com>
2022-01-11 00:08:59 +0900

    feat: redis cache config

--- src/main/java/example/redis/redisexample/RedisExampleApplication.java
+++ src/main/java/example/redis/redisexample/RedisExampleApplication.java
@@ -2,7 +2,9 @@ package example.redis.redisexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class RedisExampleApplication {
 
