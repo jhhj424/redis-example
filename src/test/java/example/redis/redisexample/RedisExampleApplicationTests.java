1589de384d481dd714b0a9a315df49fa2b49f99f
jiho Shin <jhhj424@naver.com>
2022-01-10 23:54:44 +0900

    Initial commit

---
+++ src/test/java/example/redis/redisexample/RedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package example.redis.redisexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
