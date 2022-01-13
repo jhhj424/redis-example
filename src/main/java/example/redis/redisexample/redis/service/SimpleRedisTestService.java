1505ca806e1e3541744e1cb58d9eb5fb0d5f4743
zito-s <jhhj424@naver.com>
2022-01-13 11:56:29 +0900

    feat: add SimpleRedisTestService

---
+++ src/main/java/example/redis/redisexample/redis/service/SimpleRedisTestService.java
@@ -0,0 +1,31 @@
+package example.redis.redisexample.redis.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class SimpleRedisTestService {
+
+    @Cacheable(value = "simpleData")
+    public List<String> getSimpleData(String param) {
+        log.info("start getSimpleData");
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        return list;
+    }
+
+    @CacheEvict(value = "simpleData", allEntries = true)
+    public void simpleDataEvict() {
+        log.info("cache evict");
+    }
+
+}
