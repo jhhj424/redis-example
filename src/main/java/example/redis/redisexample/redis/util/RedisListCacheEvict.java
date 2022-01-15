f6a7548205b5f3fcd3369cfec75683d049024efe
zito-s <jhhj424@naver.com>
2022-01-15 21:47:11 +0900

    feat: add annotation(RedisListCacheable, RedisListCacheEvict)

---
+++ src/main/java/example/redis/redisexample/redis/util/RedisListCacheEvict.java
@@ -0,0 +1,13 @@
+package example.redis.redisexample.redis.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RedisListCacheEvict {
+    String value();
+
+}
---
+++ src/main/java/example/redis/redisexample/redis/util/RedisListCacheable.java
@@ -0,0 +1,12 @@
+package example.redis.redisexample.redis.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RedisListCacheable {
+    String value();
+}
