98b0d6ff6789ada6c0aba294b96abcb5d040a547
zito-s <jhhj424@naver.com>
2022-01-16 00:20:28 +0900

    feat: add ContentRepository

--- src/main/java/example/redis/redisexample/redis/domain/ContentRepository.java
+++ src/main/java/example/redis/redisexample/redis/domain/ContentRepository.java
@@ -1,6 +1,14 @@
 package example.redis.redisexample.redis.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ContentRepository extends JpaRepository<Content, String> {
+    @Query("SELECT c FROM Content c WHERE c.contentName like %:contentName%")
+    public List<Content> findByContentName(@Param("contentName") String contentName);
 }
